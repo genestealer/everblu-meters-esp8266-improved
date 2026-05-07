cd0104f46c8f4dcd1d0d7b21a3863c2efbdf5a29
b4dpxl <26406101+b4dpxl@users.noreply.github.com>
2026-05-07 08:41:13 +0100

    case insensitive

--- ESPHOME-release/everblu_meter/meter_reader.cpp
+++ ESPHOME-release/everblu_meter/meter_reader.cpp
@@ -520,21 +520,22 @@ bool MeterReader::isReadingDayForConfiguredSchedule(const struct tm *ptm) const
         return true;
     }
     // check for single day reading schedule
-    switch(dayOfWeek) {
+    switch(dayOfWeek) 
+    {
         case 0:
-            return strcmp(schedule, "Sunday") == 0;
+            return strcasecmp(schedule, "sunday") == 0;
         case 1:
-            return strcmp(schedule, "Monday") == 0;
+            return strcasecmp(schedule, "monday") == 0;
         case 2:
-            return strcmp(schedule, "Tuesday") == 0;
+            return strcasecmp(schedule, "tuesday") == 0;
         case 3:
-            return strcmp(schedule, "Wednesday") == 0;
+            return strcasecmp(schedule, "wednesday") == 0;
         case 4:
-            return strcmp(schedule, "Thursday") == 0;
+            return strcasecmp(schedule, "thursday") == 0;
         case 5:
-            return strcmp(schedule, "Friday") == 0;
+            return strcasecmp(schedule, "friday") == 0;
         case 6:
-            return strcmp(schedule, "Saturday") == 0;
+            return strcasecmp(schedule, "saturday") == 0;
     }
 
     // Unknown schedule: log warning and skip read to avoid misconfiguration
